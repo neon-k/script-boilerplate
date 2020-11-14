62b62e2ea6421f8569ef31547419cc85442f5a7d
市川功太 <daiya50.ezweb.ne.jp@gmail.com>
2020-11-14 17:35:25 +0900

    update: ignoreを追加

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,8 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const test_1 = require("./test/test");
 function hello(name) {
     return `Hello, ${name}!`;
 }
-console.log(hello("World"));
+console.log(hello(test_1.test));
 //# sourceMappingURL=index.js.map
