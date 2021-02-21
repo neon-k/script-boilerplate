d166ebb3179b574536d5568d0fb4173355f0241f
市川功太 <daiya50.ezweb.ne.jp@gmail.com>
2021-02-21 18:58:32 +0900

    update: remove dist

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const test_1 = require("./test/test");
-function hello(name) {
-    return `Hello, ${name}!`;
-}
-console.log(hello(test_1.test));
-//# sourceMappingURL=index.js.map
